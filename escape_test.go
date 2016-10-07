6a9d9235c07337df29058b9f98aaa8b129d0f362
Olivier Mengué <dolmen@cpan.org>
2016-10-08 00:10:59 +0200

    Refactor TestEscape

--- escape_test.go
+++ escape_test.go
@@ -5,31 +5,35 @@ import (
 )
 
 func checkEscape(t *testing.T, in, expected string) {
-	got := Escape(in)
-	if got != expected {
-		t.Errorf("in: %#v\n    got: %#v\nexpected: %#v\n", in, got, expected)
-	} else {
-		t.Logf("%#v => %#v\n", in, got)
-	}
 }
 
 func TestEscape(t *testing.T) {
-	checkEscape(t, "", "")
-	checkEscape(t, "a", "a")
-	checkEscape(t, "a~", "a~0")
-	checkEscape(t, "~a", "~0a")
-	checkEscape(t, "a~b", "a~0b")
-	checkEscape(t, "a/", "a~1")
-	checkEscape(t, "/a", "~1a")
-	checkEscape(t, "a/b", "a~1b")
-	checkEscape(t, "a/~b", "a~1~0b")
-	checkEscape(t, "a~/b", "a~0~1b")
-	checkEscape(t, "a~~~b", "a~0~0~0b")
-	checkEscape(t, "a///b", "a~1~1~1b")
-	checkEscape(t, "a/b/c/d", "a~1b~1c~1d")
-	checkEscape(t, "é~é", "é~0é")
-	checkEscape(t, "é~", "é~0")
-	checkEscape(t, "~é", "~0é")
+	for _, tc := range []struct {
+		in, expected string
+	}{
+		{"", ""},
+		{"a", "a"},
+		{"a~", "a~0"},
+		{"~a", "~0a"},
+		{"a~b", "a~0b"},
+		{"a/", "a~1"},
+		{"/a", "~1a"},
+		{"a/b", "a~1b"},
+		{"a/~b", "a~1~0b"},
+		{"a~/b", "a~0~1b"},
+		{"a~~~b", "a~0~0~0b"},
+		{"a///b", "a~1~1~1b"},
+		{"a/b/c/d", "a~1b~1c~1d"},
+		{"é~é", "é~0é"},
+		{"é~", "é~0"},
+		{"~é", "~0é"},
+	} {
+		t.Logf("%#v => %#v\n", tc.in, tc.expected)
+		got := Escape(tc.in)
+		if got != tc.expected {
+			t.Errorf("got: %#v\n", got)
+		}
+	}
 }
 
 func TestUnescape(t *testing.T) {
