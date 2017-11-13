c26757f608ef9b7964cc436e800d9b81926458bb
Olivier Mengué <dolmen@cpan.org>
2017-11-14 00:34:27 +0100

    Add missing copyright header ; bump copyright year

--- escape.go
+++ escape.go
@@ -1,3 +1,7 @@
+// Copyright 2016-2017 Olivier Mengué. All rights reserved.
+// Use of this source code is governed by the Apache 2.0 license that
+// can be found in the LICENSE file.
+
 package jsonptr
 
 // AppendEscape appends the escaped name to dst and returns it.
