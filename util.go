a8c09d9edd79053fee881b7b010c420e211a838c
Olivier Mengué <dolmen@cpan.org>
2026-09-16 17:48:34 +0200

    Update copyright headers

--- util.go
+++ util.go
@@ -1,3 +1,7 @@
+// Copyright 2019-2026 Olivier Mengué. All rights reserved.
+// Use of this source code is governed by the Apache 2.0 license that
+// can be found in the LICENSE file.
+
 package jsonptr
 
 // MustValue allows to wrap a call to some jsonptr function which returns a value to transform any error into a panic.
--- util_test.go
+++ util_test.go
@@ -1,3 +1,7 @@
+// Copyright 2019-2026 Olivier Mengué. All rights reserved.
+// Use of this source code is governed by the Apache 2.0 license that
+// can be found in the LICENSE file.
+
 package jsonptr_test
 
 import (
