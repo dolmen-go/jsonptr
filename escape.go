ebdf5d4a6e5b2f2bf0283955f6c3a36a6bbb7676
Olivier Mengué <dolmen@cpan.org>
2026-09-23 15:17:21 +0200

    Unescape: fix godoc to match UnescapeString
    Fix doc about error in case of '/' in input.

--- escape.go
+++ escape.go
@@ -99,7 +99,7 @@ func EscapeString(name string) string {
 //	`~0` => '~'
 //
 // Any '~' followed by something else (or nothing) is an error [ErrSyntax].
-// Any '/' is an error ErrSyntax.
+// If the input contains '/' the result is undefined (may panic).
 func Unescape(b []byte) ([]byte, error) {
 	p := -1
 Loop:
